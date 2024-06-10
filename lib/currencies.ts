50542c3e6a1e951296026b122d765af77cabbd1f
Nalin216127 <nalinkeshav99@gmail.com>
2024-06-10 23:01:22 +0530

    deploy vercel

---
+++ lib/currencies.ts
@@ -0,0 +1,10 @@
+export const Currencies = [
+    { value: "INR", label: "₹ Indian Rupee", locale: "en-IN" },
+    { value: "USD", label: "$ Dollar", locale: "en-US" },
+    { value: "EUR", label: "€ Euro", locale: "de-DE" },
+    { value: "JPY", label: "¥ Yen", locale: "ja-JP" },
+    { value: "AUD", label: "$ Australian Dollar", locale: "en-AU" },
+    { value: "GBP", label: "£ Pound", locale: "en-GB" },
+  ];
+  
+  export type Currency = (typeof Currencies)[0];
